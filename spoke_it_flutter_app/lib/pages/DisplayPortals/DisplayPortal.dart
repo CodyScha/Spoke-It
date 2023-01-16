24ad753cf9aa533f13886216c2c641859a704a14
kademps <kademps@gmail.com>
2023-01-15 20:19:59 -0600

    edited ui

---
+++ spoke_it_flutter_app/lib/pages/DisplayPortals/DisplayPortal.dart
@@ -0,0 +1,37 @@
+/*import 'dart:io';
+
+import 'package:flutter/material.dart';
+import '../load/LoadView.dart';
+import 'package:filesystem_picker/filesystem_picker.dart';
+
+class Display extends StatelessWidget {
+  const Display({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Spoke-It', //web name
+      theme: ThemeData(
+        // This is the theme of your application.
+
+        primarySwatch: Colors.indigo, //title color
+      ),
+      home: myDisplay(title: 'Prototype: Spoke-It'), //displayed title
+    );
+  }
+}
+
+class myDisplay extends StatefulWidget {
+  const myDisplay({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<myDisplay> createState() => _myDisplay();
+}
+
+class _myDisplay extends State<myDisplay> {
+
+
+}
+*/
