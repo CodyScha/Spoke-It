73f43a89dd62f6cfc56a50f47fe734c249e6e8da
CodyScha <codscha@siue.edu>
2023-02-07 16:03:31 -0600

    preview page push

---
+++ spoke_it_flutter_app/lib/pages/preview/previewPage.dart
@@ -0,0 +1,110 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import '../load/LoadView.dart';
+import 'package:filesystem_picker/filesystem_picker.dart';
+import '../home/homeView.dart';
+
+class Preview extends StatelessWidget {
+  const Preview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    String dir = Directory.current.toString();
+    return MaterialApp(
+      title: 'Spoke-It', //web name
+      theme: ThemeData(
+        // This is the theme of your application.
+
+        primarySwatch: Colors.indigo, //title color
+      ),
+      home: myPreview(
+          file: File('$dir/Saved/SIUE_Gardens.txt')), //displayed title
+    );
+  }
+}
+
+class myPreview extends StatefulWidget {
+  const myPreview({super.key, required this.file});
+
+  final File file;
+
+  @override
+  State<myPreview> createState() => _myPreview();
+}
+
+class _myPreview extends State<myPreview> {
+  late String test;
+
+  void initState() {
+    test = 'fortnite';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //top bar
+        title: Text('Preview'),
+        centerTitle: true, //centers text
+      ),
+      body: Row(
+        children: [
+          Column(
+            // Center is a layout widget. It takes a single child and positions it
+            // in the middle of the parent.
+            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            // crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              // Container(
+              //   color: Color.fromARGB(255, 7, 175, 7),
+              //   padding:
+              //       EdgeInsets.symmetric(vertical: 20.0, horizontal: 120.0),
+              //   //child: Text('Load Data'),
+              // ),
+              // Container(
+              //   color: Color.fromARGB(255, 173, 16, 16),
+              //   padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 92.0),
+              //   child: TextButton(
+              //       onPressed: () {
+              //         Navigator.push(
+              //           context,
+              //           MaterialPageRoute(
+              //               builder: (context) =>
+              //                   MyHomePage(title: 'Prototype: Spoke-It')),
+              //         );
+              //       },
+              //       child: Text('')),
+              // ),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(5),
+                child: Stack(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.all(15),
+                      child: TextButton(
+                      onPressed: () {
+                        print('pressed da $test button');
+                      },
+                      child: Text(test),
+                      style: TextButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        textStyle: const TextStyle(fontSize: 45),
+                        backgroundColor: Colors.pink
+                      ),),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              Text('hello :P')
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
