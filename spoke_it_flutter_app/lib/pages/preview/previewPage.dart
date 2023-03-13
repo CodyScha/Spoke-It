18a67c4711dee83345c808b045c85dfe0367ce95
Kristina Tschomakoff <ktschom@siue.edu>
2023-03-12 21:20:05 -0500

    added save Popup to save custom filename

--- spoke_it_flutter_app/lib/pages/preview/previewPage.dart
+++ spoke_it_flutter_app/lib/pages/preview/previewPage.dart
@@ -143,8 +143,10 @@ class _myPreview extends State<myPreview> {
           border: Border.all(
               color: Color.fromARGB(255, 117, 209, 255),
               width: 4,
-              style: BorderStyle.solid,
-              strokeAlign: StrokeAlign.outside)),
+              style: BorderStyle.solid
+              // ,
+              // strokeAlign: StrokeAlign.outside
+              )),
     );
 
     _selectedHiddenPortal = Container(
@@ -156,8 +158,10 @@ class _myPreview extends State<myPreview> {
           border: Border.all(
               color: Color.fromARGB(255, 117, 209, 255),
               width: 4,
-              style: BorderStyle.solid,
-              strokeAlign: StrokeAlign.outside)),
+              style: BorderStyle.solid
+              // ,
+              // strokeAlign: StrokeAlign.outside
+              )),
     );
   }
 
