9cd1f6f805f8d866bd8cce6661026cd10a3fcd76
sdelozi <sdelozi@siue.edu>
2023-03-14 11:30:04 -0500

    implementing maxWedge

--- spoke_it_flutter_app/lib/source/spoke.dart
+++ spoke_it_flutter_app/lib/source/spoke.dart
@@ -1,4 +1,5 @@
 import 'package:spoke_it_flutter_app/source/portals.dart';
+import 'dart:math';
 
 class Spoke {
   List<Link> algorithm(List<Portal> portals) {
@@ -9,7 +10,7 @@ class Spoke {
     List<Portal> shownPortalList = shownPortals(portals);
 
     //calculate links in hull
-    links.addAll(jarvis(shownPortalList));
+    links.addAll(jarvis(shownPortalList)[0]);
 
     //connect hull to center
     links.addAll(hullToCenter(shownPortalList));
@@ -23,6 +24,7 @@ class Spoke {
     return links;
   }
 
+  //returns list of portals that are not hidden since we don't want to include these in the algorithm
   List<Portal> shownPortals(List<Portal> portals) {
     List<Portal> shownPortals = [];
 
@@ -33,7 +35,8 @@ class Spoke {
     return shownPortals;
   }
 
-  List<Link> jarvis(List<Portal> portals) {
+  //returns tuple with list of links of the convex hull and the number of portals in the convex hull
+  List jarvis(List<Portal> portals) {
     List<Link> links = [];
     double val;
     List<Portal> hullList = [];
@@ -96,7 +99,7 @@ class Spoke {
         links.add(link);
       }
     }
-    return links;
+    return [links, hullList.length];
   }
 
   List<Link> hullToCenter(List<Portal> portals) {
@@ -146,6 +149,48 @@ class Spoke {
   List<Link> internalLinks(List<Portal> portals) {
     List<Link> links = [];
 
+    //for all wedges in G, call maxWedge(list of portals in wedge, hull point 1, hull point 2)
+
     return links;
+  }
+
+  List<Link> maxWedge(List<Portal> portals, Portal wedgeOne, Portal wedgeTwo) {
+    List<Link> links = [];
+    //checking if there are portals within the wedge (base case is empty wedge)
+    if (portals.length != 3) {
+      //find farthest portal from center (P) in the wedge
+      //link to wedge 1 and wedge 2
+      //links.addAll(maxWedge(left wedge portals, wedgeOne, P))
+      //links.addAll(maxWedge(right wedge portals, P, wedgeTwo))
+    }
+
+    return links;
+  }
+
+  Portal findFurthestFromCenter(List<Portal> portals) {
+    Portal furthest, center;
+    double distance = 0;
+    double xDif, yDif;
+
+    //initialize non-nullable furthest and center (will be overridden)
+    center = portals[0];
+    furthest = portals[0];
+
+    //find center portal
+    for (Portal portal in portals) {
+      if (portal.center == true) center = portal;
+    }
+
+    //find furthest from center
+    for (Portal portal in portals) {
+      xDif = center.lat - portal.lat;
+      yDif = center.long - portal.long;
+      if (sqrt(pow(xDif, 2) + pow(yDif, 2)) > distance) {
+        distance = sqrt(pow(xDif, 2) + pow(yDif, 2));
+        furthest = portal;
+      }
+    }
+
+    return furthest;
   }
 }
