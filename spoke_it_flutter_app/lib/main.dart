1c4f0de52b81923ca3d4d77054b9b09634d26a6e
sdelozi <sdelozi@siue.edu>
2023-04-10 19:38:47 -0500

    window title

--- spoke_it_flutter_app/lib/main.dart
+++ spoke_it_flutter_app/lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    setWindowTitle('Spoke-It v2.1');
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
